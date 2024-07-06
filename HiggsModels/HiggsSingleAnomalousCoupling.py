7496449df0fcad5d28dca1463263cce4c0d849d5
bmjoshi <joshi.bm163@gmail.com>
2024-07-06 22:14:41 +0530

    Update HiggsSingleAnomalousCoupling.py

--- HiggsModels/HiggsSingleAnomalousCoupling.py
+++ HiggsModels/HiggsSingleAnomalousCoupling.py
@@ -81,7 +81,7 @@ class FA3_Interference_JHU_ggHSyst_rw_MengsMuV_HeshyXsec_ggHInt_ggHphase(Physics
 
     def getYieldScale(self,bin,process):
         if not self.DC.isSignal[process]: return 1
-        years = ["2016preVFP","2016postVFP","2017","2018"]
+        years = ["2016preVFP","2016postVFP", "2017","2018"]
         scale = '1'
 
         if process in ["ggH_%s_hgg"%y for y in years]:
@@ -111,5 +111,201 @@ class FA3_Interference_JHU_ggHSyst_rw_MengsMuV_HeshyXsec_ggHInt_ggHphase(Physics
         print "Bin/Process %s/%s will get scaled by %s"%(bin,process,scale)
         return scale
 
+class FA2_Interference_JHU_rw_MengsMuV(PhysicsModel):
+    def __init__(self):
+        self.altSignal = "ALT_0PH"
+
+    def setPhysicsOptions(self,physOptions):
+        for po in physOptions:
+            if po.startswith("altSignal="): self.altSignal = po.split("=")[1]
+
+    def doParametersOfInterest(self):
+        """Create POI and other parameters, and define the POI set."""
+        xsecs = {
+            "sigma1_HZZ": 290.58626,
+            #          "sigma3_HZZ": 581.17253,
+            "sigma3_HZZ": 44.670158,
+            "sigma1_VBF": 968.674,
+            "sigma3_VBF": 10909.54,
+            "sigma1_ZH":  9022.36,
+            "sigma3_ZH":  434763.7,
+            "sigma1_WH":  30998.54,
+            "sigma3_WH":  2028656,            
+            "sigma2_HZZ": 105.85594,
+            "sigmaL1_HZZ": 1.9846071e-06,
+            "sigma2_VBF": 13102.71,
+            "sigmaL1_VBF": 2.08309E-4,
+            "sigma2_ZH": 713123,
+            "sigmaL1_ZH": 33652.46e-6,
+            "sigma2_WH": 3106339,
+            "sigmaL1_WH": 11234.91e-5,
+            "sigmaa1a3int_VBF": 0,
+            "sigmaa1a3int_ZH": 0,
+            "sigmaa1a3int_WH": 0,
+            "sigmaa1a2int_VBF": 2207.73 ,
+            "sigmaa1a2int_ZH": 4258.966,
+            "sigmaa1a2int_WH": 16486.68,
+            "sigmaa1L1int_VBF": 923.86500,
+            "sigmaa1L1int_ZH": -11192.413,
+            "sigmaa1L1int_WH": -36694.71,
+        }
+
+        self.modelBuilder.doVar("CMS_zz4l_fai1[0.0,-1.0,1.0]");
+#        self.modelBuilder.doVar("fa3_ggH[0.0,0.0,1.0]");
+        self.modelBuilder.doVar("muf[1.0,0,10]");
+        self.modelBuilder.doVar("muV[1.0,0.0,10.0]");
+        self.modelBuilder.doSet("POI","CMS_zz4l_fai1,muV,muf")
+#        self.modelBuilder.out.var("muf").setAttribute("flatParam")
+        
+        self.modelBuilder.doVar('expr::a1("sqrt(1-abs(@0))", CMS_zz4l_fai1)')
+        self.modelBuilder.doVar('expr::a3("(@0>0 ? 1 : -1) * sqrt(abs(@0)*{sigma1_HZZ}/{sigma2_HZZ})", CMS_zz4l_fai1)'.format(**xsecs))
+
+
+        self.modelBuilder.factory_('expr::muVc("@1/(1+200.*abs(@0))", CMS_zz4l_fai1,muV)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_VBF("@0*@1**2 - @0*@1*@2*sqrt({sigma2_VBF}/{sigma1_VBF})", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_ZH("@0*@1**2 - @0*@1*@2*sqrt({sigma2_ZH}/{sigma1_ZH})", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_WH("@0*@1**2 - @0*@1*@2*sqrt({sigma2_WH}/{sigma1_WH})", muVc,a1,a3)'.format(**xsecs))
+
+
+        self.modelBuilder.factory_('expr::bsmCoupling_VBF("@0*@1**2*{sigma2_VBF}/{sigma1_VBF} - @0*@1*@2*sqrt({sigma2_VBF}/{sigma1_VBF})", muVc,a3,a1)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::bsmCoupling_ZH("@0*@1**2*{sigma2_ZH}/{sigma1_ZH} - @0*@1*@2*sqrt({sigma2_ZH}/{sigma1_ZH})", muVc,a3,a1)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::bsmCoupling_WH("@0*@1**2*{sigma2_WH}/{sigma1_WH} - @0*@1*@2*sqrt({sigma2_WH}/{sigma1_WH})", muVc,a3,a1)'.format(**xsecs))
+
+
+        self.modelBuilder.factory_('expr::intCoupling_VBF("@0*@1*@2*sqrt({sigma2_VBF}/{sigma1_VBF})*{sigmaa1a2int_VBF}/{sigma1_VBF}", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::intCoupling_ZH("@0*@1*@2*sqrt({sigma2_ZH}/{sigma1_ZH})*{sigmaa1a2int_ZH}/{sigma1_ZH}", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::intCoupling_WH("@0*@1*@2*sqrt({sigma2_WH}/{sigma1_WH})*{sigmaa1a2int_WH}/{sigma1_WH}", muVc,a1,a3)'.format(**xsecs))
+
+
+    def getYieldScale(self,bin,process):
+        if not self.DC.isSignal[process]: return 1
+        years = ["2016preVFP","2016postVFP", "2017","2018"]
+        scale = '1'
+
+        if process in ["ggH_%s_hgg"%y for y in years]:
+            scale = 'smCoupling_ggH'
+        if process in ["ggH_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_ggH'
+        if process in ["qqH_%s_hgg"%y for y in years]:
+            scale = 'smCoupling_VBF'
+        if process in ["wh_ALT_0PM_%s_hgg"%y for y in years]: # should separate W and Z
+            scale = 'smCoupling_WH'
+        if process in ["zh_ALT_0PM_%s_hgg"%y for y in years]: # not yet there
+            scale = 'smCoupling_ZH'
+        if process in ["qqH_%s_%s_hgg"%(self.altSignal,y) for y in years]:
+            scale = 'bsmCoupling_VBF'
+        if process in ["wh_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_WH'
+        if process in ["zh_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_ZH'
+        if process in ["qqH_%sf05_%s_hgg"%(self.altSignal,y) for y in years]:
+            scale = 'intCoupling_VBF'
+        if process in ["zh_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'intCoupling_ZH'
+        if process in ["wh_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered 
+            scale = 'intCoupling_WH'
+        if process in ["ggH_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'intCoupling_ggH'
+        print "Bin/Process %s/%s will get scaled by %s"%(bin,process,scale)
+        return scale
+
+class FL1_Interference_JHU_rw_MengsMuV(PhysicsModel):
+    def __init__(self):
+        self.altSignal = "ALT_0L1"
+
+    def setPhysicsOptions(self,physOptions):
+        for po in physOptions:
+            if po.startswith("altSignal="): self.altSignal = po.split("=")[1]
+
+    def doParametersOfInterest(self):
+        """Create POI and other parameters, and define the POI set."""
+        xsecs = {
+            "sigma1_HZZ": 290.58626,
+            #          "sigma3_HZZ": 581.17253,
+            "sigma3_HZZ": 44.670158,
+            "sigma1_VBF": 968.674,
+            "sigma3_VBF": 10909.54,
+            "sigma1_ZH":  9022.36,
+            "sigma3_ZH":  434763.7,
+            "sigma1_WH":  30998.54,
+            "sigma3_WH":  2028656,            
+            "sigma2_HZZ": 105.85594,
+            "sigmaL1_HZZ": 1.9846071e-06,
+            "sigma2_VBF": 13102.71,
+            "sigmaL1_VBF": 2.08309E-4,
+            "sigma2_ZH": 713123,
+            "sigmaL1_ZH": 33652.46e-6,
+            "sigma2_WH": 3106339,
+            "sigmaL1_WH": 11234.91e-5,
+            "sigmaa1a3int_VBF": 0,
+            "sigmaa1a3int_ZH": 0,
+            "sigmaa1a3int_WH": 0,
+            "sigmaa1a2int_VBF": 2207.73 ,
+            "sigmaa1a2int_ZH": 4258.966,
+            "sigmaa1a2int_WH": 16486.68,
+            "sigmaa1L1int_VBF": 2861.21349769,
+            "sigmaa1L1int_ZH": 6852.307,
+            "sigmaa1L1int_WH": 25302.37,
+
+        }
+
+        self.modelBuilder.doVar("CMS_zz4l_fai1[0.0,-1.0,1.0]");
+#        self.modelBuilder.doVar("fa3_ggH[0.0,0.0,1.0]");
+        self.modelBuilder.doVar("muf[1.0,0,10]");
+        self.modelBuilder.doVar("muV[1.0,0.0,10.0]");
+        self.modelBuilder.doSet("POI","CMS_zz4l_fai1,muV,muf")
+#        self.modelBuilder.out.var("muf").setAttribute("flatParam")
+        
+        self.modelBuilder.doVar('expr::a1("sqrt(1-abs(@0))", CMS_zz4l_fai1)')
+        self.modelBuilder.doVar('expr::a3("(@0>0 ? 1 : -1) * sqrt(abs(@0)*{sigma1_HZZ}/{sigmaL1_HZZ})", CMS_zz4l_fai1)'.format(**xsecs))
+
+        self.modelBuilder.factory_('expr::muVc("@1/(1+500.*abs(@0))", CMS_zz4l_fai1,muV)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_VBF("@0*@1**2 - @0*@1*@2*sqrt({sigmaL1_VBF}/{sigma1_VBF})", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_ZH("@0*@1**2 - @0*@1*@2*sqrt({sigmaL1_ZH}/{sigma1_ZH})", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::smCoupling_WH("@0*@1**2 - @0*@1*@2*sqrt({sigmaL1_WH}/{sigma1_WH})", muVc,a1,a3)'.format(**xsecs))
+
+
+        self.modelBuilder.factory_('expr::bsmCoupling_VBF("@0*@1**2*{sigmaL1_VBF}/{sigma1_VBF} - @0*@1*@2*sqrt({sigmaL1_VBF}/{sigma1_VBF})", muVc,a3,a1)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::bsmCoupling_ZH("@0*@1**2*{sigmaL1_ZH}/{sigma1_ZH} - @0*@1*@2*sqrt({sigmaL1_ZH}/{sigma1_ZH})", muVc,a3,a1)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::bsmCoupling_WH("@0*@1**2*{sigmaL1_WH}/{sigma1_WH} - @0*@1*@2*sqrt({sigmaL1_WH}/{sigma1_WH})", muVc,a3,a1)'.format(**xsecs))
+
+
+        self.modelBuilder.factory_('expr::intCoupling_VBF("@0*@1*@2*sqrt({sigmaL1_VBF}/{sigma1_VBF})*{sigmaa1L1int_VBF}/{sigma1_VBF}", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::intCoupling_ZH("@0*@1*@2*sqrt({sigmaL1_ZH}/{sigma1_ZH})*{sigmaa1L1int_ZH}/{sigma1_ZH}", muVc,a1,a3)'.format(**xsecs))
+        self.modelBuilder.factory_('expr::intCoupling_WH("@0*@1*@2*sqrt({sigmaL1_WH}/{sigma1_WH})*{sigmaa1L1int_WH}/{sigma1_WH}", muVc,a1,a3)'.format(**xsecs))
+
+    def getYieldScale(self,bin,process):
+        if not self.DC.isSignal[process]: return 1
+        years = ["2016preVFP","2016postVFP", "2017","2018"]
+        scale = '1'
+
+        if process in ["ggH_%s_hgg"%y for y in years]:
+            scale = 'smCoupling_ggH'
+        if process in ["ggH_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_ggH'
+        if process in ["qqH_%s_hgg"%y for y in years]:
+            scale = 'smCoupling_VBF'
+        if process in ["wh_ALT_0PM_%s_hgg"%y for y in years]: # should separate W and Z
+            scale = 'smCoupling_WH'
+        if process in ["zh_ALT_0PM_%s_hgg"%y for y in years]: # not yet there
+            scale = 'smCoupling_ZH'
+        if process in ["qqH_%s_%s_hgg"%(self.altSignal,y) for y in years]:
+            scale = 'bsmCoupling_VBF'
+        if process in ["wh_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_WH'
+        if process in ["zh_%s_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'bsmCoupling_ZH'
+        if process in ["qqH_%sf05_%s_hgg"%(self.altSignal,y) for y in years]:
+            scale = 'intCoupling_VBF'
+        if process in ["zh_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'intCoupling_ZH'
+        if process in ["wh_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered 
+            scale = 'intCoupling_WH'
+        if process in ["ggH_%sf05_%s_hgg"%(self.altSignal,y) for y in years]: # not yet considered
+            scale = 'intCoupling_ggH'
+        print "Bin/Process %s/%s will get scaled by %s"%(bin,process,scale)
+        return scale    
 
 FA3_Interference_JHU_ggHSyst_rw_MengsMuV_HeshyXsec_ggHInt_ggHphase = FA3_Interference_JHU_ggHSyst_rw_MengsMuV_HeshyXsec_ggHInt_ggHphase()
+FA2_Interference_JHU_rw_MengsMuV = FA2_Interference_JHU_rw_MengsMuV()
+FL1_Interference_JHU_rw_MengsMuV = FL1_Interference_JHU_rw_MengsMuV()
